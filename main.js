66abb90fb85ee3a5dc523a0e1119e62e4e63839f
Freddy <79088826+freddy-noor@users.noreply.github.com>
2022-03-27 21:33:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function countPositivesSumNegatives(input) {
-    let posCount = input.filter(x => x > 0).length
-    let negSum = input.filter(x => x < 0).reduce((a, b) => a +  b)
-    let answer = []
-    answer.push(posCount)
-    answer.push(negSum)
-
-    return answer
+    let findPost = arr => arr > 0 ? true : false;
+    let posCount = input.filter(findPost).length;
+    let getNeg = arr => arr < 0 ? true : false;
+    let negSum = input.filter(getNeg).reduce((total, value) => total + value);
+    let answer = [];
+    answer.push(posCount);
+    answer.push(negSum);
+    return answer;
 }
 
 let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
